b18de07c75f3a94d98264e09f924cef68be83983
psupra22 <psupra521@gmail.com>
2026-03-18 15:29:12 -0400

    Add save/load string test

--- GoDoIt.Tests/UnitTest1.cs
+++ GoDoIt.Tests/UnitTest1.cs
@@ -1,15 +1,23 @@
-﻿namespace GoDoIt.Tests;
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Text;
 
-public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
+namespace GoDoIt.Tests {
+    [TestFixture]
+    public class BasicTests {
+        [Test]
+        public void SaveAndLoad_String_Works() {
+            using var stream = new MemoryStream();
+            var data = Encoding.UTF8.GetBytes("Go Do It Test");
+            stream.Write(data, 0, data.Length);
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+            stream.Position = 0;
+            var loaded = new byte[stream.Length];
+            stream.Read(loaded, 0, loaded.Length);
+
+            string result = Encoding.UTF8.GetString(loaded);
+            Assert.That(result, Is.EqualTo("Go Do It Test"));
+        }
     }
 }
