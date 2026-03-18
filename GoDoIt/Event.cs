5b8e3422b7f67966be7edad97d061c3fbc05d0d1
Charlie Norton <64992071+22cnorton@users.noreply.github.com>
2026-03-18 17:42:19 -0400

    add DueToday function to event

--- GoDoIt/Event.cs
+++ GoDoIt/Event.cs
@@ -3,5 +3,5 @@ namespace GoDoIt;
 
 public record Event(int Id, string Title, string Description, DateTime DueDate, int CategoryId, int? ParentId, bool IsComplete, TimeSpan? RepeatInterval)
 {
-
+    public bool DueToday() => throw new NotImplementedException();
 }
