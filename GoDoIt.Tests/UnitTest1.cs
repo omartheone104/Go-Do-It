78777774cdc91b4535e04c3edc634c5db44b3c07
Charlie Norton <64992071+22cnorton@users.noreply.github.com>
2026-03-18 17:43:03 -0400

    add tests for DueToday

--- GoDoIt.Tests/UnitTest1.cs
+++ GoDoIt.Tests/UnitTest1.cs
@@ -1,13 +1,17 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
 using System;
 using System.IO;
 using System.Text;
 
-namespace GoDoIt.Tests {
+namespace GoDoIt.Tests
+{
     [TestFixture]
-    public class BasicTests {
+    public class BasicTests
+    {
         [Test]
-        public void SaveAndLoad_String_Works() {
+        public void SaveAndLoad_String_Works()
+        {
             using var stream = new MemoryStream();
             var data = Encoding.UTF8.GetBytes("Go Do It Test");
             stream.Write(data, 0, data.Length);
@@ -18,6 +22,28 @@ namespace GoDoIt.Tests {
 
             string result = Encoding.UTF8.GetString(loaded);
             Assert.That(result, Is.EqualTo("Go Do It Test"));
+        }
+
+        [Test]
+        public void DueToday_WhenDateIsToday_ReturnsTrue()
+        {
+            var TodayEvent = new Event(Id: 1, Title: "Today", Description: "Today Test Event", DueDate: DateTime.Today, CategoryId: 1, ParentId: null, IsComplete: false, RepeatInterval: null);
+
+            Assert.That(TodayEvent.DueToday(), Is.True);
+        }
+        [Test]
+        public void DueToday_WhenDateIsFuture_ReturnsFalse()
+        {
+            var FutureEvent = new Event(Id: 1, Title: "Future", Description: "Future Test Event", DueDate: DateTime.Today.AddDays(10), CategoryId: 1, ParentId: null, IsComplete: false, RepeatInterval: null);
+
+            Assert.That(FutureEvent.DueToday(), Is.False);
+        }
+        [Test]
+        public void DueToday_WhenDateIsPast_ReturnsFalse()
+        {
+            var PastEvent = new Event(Id: 1, Title: "Past", Description: "Past Test Event", DueDate: DateTime.Today.AddDays(-10), CategoryId: 1, ParentId: null, IsComplete: false, RepeatInterval: null);
+
+            Assert.That(PastEvent.DueToday(), Is.False);
         }
     }
 }
