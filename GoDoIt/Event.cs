72d33cf8e918edfd1f03c374d8c7b6442101d573
Charlie Norton <64992071+22cnorton@users.noreply.github.com>
2026-03-18 16:30:50 -0400

    fix: add missing dependency

--- GoDoIt/Event.cs
+++ GoDoIt/Event.cs
@@ -1,3 +1,4 @@
+using System;
 namespace GoDoIt;
 
 record Event(int Id, string Title, string Description, DateTime DueDate, int CategoryId, int? ParentId, bool IsComplete, TimeSpan? RepeatInterval)
