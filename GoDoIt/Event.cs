5daf62d4978f0028e06c681760be38847e521cdd
Charlie Norton <64992071+22cnorton@users.noreply.github.com>
2026-03-18 17:50:35 -0400

    add functionality for DueToday

--- GoDoIt/Event.cs
+++ GoDoIt/Event.cs
@@ -3,5 +3,9 @@ namespace GoDoIt;
 
 public record Event(int Id, string Title, string Description, DateTime DueDate, int CategoryId, int? ParentId, bool IsComplete, TimeSpan? RepeatInterval)
 {
-    public bool DueToday() => throw new NotImplementedException();
+    /// <summary>
+    /// Checks if the event is due today, when the event is not complete and the DueDate is the same as the current date
+    /// </summary>
+    /// <returns>true if event is Due, false otherwise</returns>
+    public bool DueToday() => !IsComplete && (DueDate == DateTime.Today);
 }
