c7849c830fe84103d0c6b976eae9bbe79cf9e1ed
Omar Aly <79159097+omartheone104@users.noreply.github.com>
2026-03-18 17:01:23 -0400

    Revert "Event class and Category Class"

--- GoDoIt/Event.cs
+++
@@ -1,7 +0,0 @@
-using System;
-namespace GoDoIt;
-
-record Event(int Id, string Title, string Description, DateTime DueDate, int CategoryId, int? ParentId, bool IsComplete, TimeSpan? RepeatInterval)
-{
-
-}
