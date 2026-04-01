68f35506ac01140dfa6fc67c3201fcfb51225ed4
Porter02034542 <Bennett_Porter@student.uml.edu>
2026-04-01 00:22:25 -0400

    added script and readme and dummy csv to generate table

---
+++ Results/table.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+df = pd.read_csv("./data.csv")
+
+fig = go.Figure(
+    data=[
+        go.Table(
+            header=dict(
+                values=list(df.columns),
+                fill_color="lightblue",
+                align="center",
+                font=dict(color="black", size=14)
+            ),
+            cells=dict(
+                values=[df[col] for col in df.columns],
+                align="center",
+                font=dict(color="black", size=12)
+            )
+        )
+    ]
+)
+
+# Show the table in a browser or notebook
+fig.show()
