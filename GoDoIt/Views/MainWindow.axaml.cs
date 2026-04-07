70d02e2af22eab8468eb15cc41254a03d419ea11
psupra22 <psupra521@gmail.com>
2026-04-07 17:13:41 -0400

    Add custom categories

--- GoDoIt/Views/MainWindow.axaml.cs
+++ GoDoIt/Views/MainWindow.axaml.cs
@@ -1,5 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Media;
+using GoDoIt;
 using GoDoIt.ViewModels;
 
 namespace GoDoIt.Views;
@@ -10,13 +12,31 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
-    
+
     private void OnTaskCardPressed(object? sender, PointerPressedEventArgs e)
     {
-        if (sender is Border { Tag: EventViewModel evm } && 
-            DataContext is MainWindowViewModel vm) 
-        { 
+        if (sender is Border { Tag: EventViewModel evm } &&
+            DataContext is MainWindowViewModel vm)
+        {
             vm.SelectTaskCommand.Execute(evm);
+        }
+    }
+
+    private void OnDeleteCategoryClicked(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (sender is Button { Tag: Category cat } &&
+            DataContext is MainWindowViewModel vm)
+        {
+            vm.DeleteCategoryCommand.Execute(cat);
+        }
+    }
+
+    private void OnColorSwatchPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (sender is Border { Tag: Color color } && 
+            DataContext is MainWindowViewModel vm)
+        { 
+            vm.NewCategory.SelectedColor = color;
         } 
     }
 }
